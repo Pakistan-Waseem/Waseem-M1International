63a14470e89597acd4659b6b94f951e8b5cd2389
waseem <shahzadw38@gmail.com>
2015-04-15 00:17:19 +0200

    First Commit

---
+++ M1International/src/Main.java
@@ -0,0 +1,26 @@
+
+
+/**
+ * 
+ */
+
+
+/**
+ * @author waseem
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println( "Hello World!" );
+        GuavaMultimap tester = new GuavaMultimap();
+        tester.testMultimap();
+        GuavaBimap test1=new GuavaBimap();
+        test1.testBimap();
+	}
+
+}
